e60d27c3e534a39866ce98022a551f165966f4d1
AL3X-69 <26258257+AL3X-69@users.noreply.github.com>
2023-10-17 00:47:44 +0200

    Préparation TP 6

---
+++ pi.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+#define MAX_ITERATIONS 100
+
+int main() {
+    double x, y, lpi;
+    int i;
+
+    x = sqrt(2.);
+    lpi = 2. + x;
+    y = pow(2., 1./4.);
+    for (i = 0; i < MAX_ITERATIONS; ++i) {
+        x = (sqrt(x) + 1.0 / sqrt(x)) / 2.0;
+        lpi *= (x + 1.0) / (y + 1.0);
+        y = (y * sqrt(x) + 1.0 / sqrt(x)) / (y + 1.0);
+    }
+    printf("pi = %f\n", lpi);
+}
